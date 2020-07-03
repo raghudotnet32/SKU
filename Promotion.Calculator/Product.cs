3e1071aeb3b3bc335647dcff50004db7b126d389
raghudotnet32 <Jagadesh.Jolapalli@appliedis.com>
2020-07-03 13:20:39 +0530

    Added Product model

---
+++ Promotion.Calculator/Product.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Promotion.Calculator
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public int Count { get; set; }
+        
+
+        public Product(string input)
+        {
+            var itemDetails = input.Split('*');
+            Id = itemDetails[1];
+            Count = Convert.ToInt32(itemDetails[0]);
+        }
+
+        public Product(Tuple<string, int> input)
+        {
+            Id = input.Item1;
+            Count = input.Item2;
+        }
+
+        public Product(string productId, int count)
+        {
+            Id = productId;
+            Count = count;
+        }
+    }
+}
