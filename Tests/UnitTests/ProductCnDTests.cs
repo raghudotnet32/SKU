45fd9284e338f99a46d15b41b2be7e88cc54043c
raghudotnet32 <Jagadesh.Jolapalli@appliedis.com>
2020-07-03 15:31:55 +0530

    Added Test cases for ProductCnD to validate Single product count

--- Tests/UnitTests/ProductCnDTests.cs
+++ Tests/UnitTests/ProductCnDTests.cs
@@ -24,6 +24,34 @@ namespace Promotion.Calculator.Tests
             value.Equals(30);
         }
 
+        [Fact(DisplayName = "ProductCnDTestTotalPrice_Returns_ProductC_NonPromotion_Price")]
+        public void ProductCnDTestTotalPrice_Returns_ProductC_NonPromotion_Price()
+        {
+            //Arrange
+            List<Product> products = new List<Product>();
+            products.Add(new Product("C", 1));
+
+            //Assert
+            var value = new ProductCnD<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(20);
+        }
+
+        [Fact(DisplayName = "ProductCnDTestTotalPrice_Returns_ProductD_NonPromotion_Price")]
+        public void ProductCnDTestTotalPrice_Returns_ProductD_NonPromotion_Price()
+        {
+            //Arrange
+            List<Product> products = new List<Product>();
+            products.Add(new Product("D", 1));
+
+            //Assert
+            var value = new ProductCnD<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(15);
+        }
+
         [Fact(DisplayName = "ProductCnDTestTotalPrice_Returns_Zero_OnNoProduct")]
         public void ProductCnDTestTotalPrice_Returns_Zero_OnNoProduct()
         {
