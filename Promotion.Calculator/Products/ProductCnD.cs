21c15df6c25b34e4045bf6ba9cd1f00bb9ab8223
raghudotnet32 <Jagadesh.Jolapalli@appliedis.com>
2020-07-03 14:55:51 +0530

    Added code to ProductCnD and test cases

--- Promotion.Calculator/Products/ProductCnD.cs
+++ Promotion.Calculator/Products/ProductCnD.cs
@@ -5,14 +5,42 @@ namespace Promotion.Calculator.Products
 {
     public class ProductCnD<T> : IPriceCalculator<T>, IPromotion<T> where T : List<Product>
     {
+        private readonly int priceOfC = 20;
+        private readonly int priceOfD = 15;
+        private readonly int priceOfCnD = 30;
+        private readonly Product productC;
+        private readonly Product productD;
+
+        public ProductCnD(T tType)
+        {
+            this.productC = tType.Where(x => x.Id == "C").SingleOrDefault();
+            this.productD = tType.Where(x => x.Id == "D").SingleOrDefault();
+        }
+
         public int ApplyPromotion()
         {
-            throw new System.NotImplementedException();
+            if (productC.Count == productD.Count)
+            {
+                return productD.Count * priceOfCnD;
+            }
+            else if (productC.Count > productD.Count)
+            {
+                return ((productC.Count - productD.Count) * priceOfC) + productD.Count * priceOfCnD;
+            }
+            else
+            {
+                return productC.Count * priceOfCnD + ((productD.Count - productC.Count) * priceOfD);
+            }
         }
 
         public int CalculateProductPrice()
         {
-            throw new System.NotImplementedException();
+            if (productC is null && productD is null)
+                return 0;
+            if (productC.Count <= 0 | productD.Count <= 0)
+                return (productC.Count * priceOfC) + (productD.Count * priceOfD);
+
+            return ApplyPromotion();
         }
     }
 }
---
+++ Tests/UnitTests/ProductCnDTests.cs
@@ -0,0 +1,46 @@
+﻿using Promotion.Calculator.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Promotion.Calculator.Tests
+{
+    public class ProductCnDTests : IDisposable
+    {
+
+        [Fact(DisplayName = "ProductCnDTestTotalPrice_Returns_Promotion_Price")]
+        public void ProductCnDTestTotalPrice_Returns_Promotion_Price()
+        {
+            //Arrange
+            List<Product> products = new List<Product>();
+            products.Add(new Product("C", 1));
+            products.Add(new Product("D", 1));
+
+            //Assert
+            var value = new ProductCnD<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(30);
+        }
+
+        [Fact(DisplayName = "ProductCnDTestTotalPrice_Returns_Zero_OnNoProduct")]
+        public void ProductCnDTestTotalPrice_Returns_Zero_OnNoProduct()
+        {
+            //Arrange
+            List<Product> products = new List<Product>();
+
+            //Assert
+            var value = new ProductCnD<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(0);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
+
