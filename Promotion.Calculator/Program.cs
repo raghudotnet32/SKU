6e7ff329f1472eac5a05ee8724b679f80b33d3fe
raghudotnet32 <Jagadesh.Jolapalli@appliedis.com>
2020-07-03 13:23:33 +0530

    Added code to Main method to read input data from user console

--- Promotion.Calculator/Program.cs
+++ Promotion.Calculator/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Promotion.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Promotion.Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static Dictionary<string, int> cart = new Dictionary<string, int>();
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("total number of order");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the type of product:A,B,C or D along with quantity Example: 3*A");
+            for (int i = 0; i < a; i++)
+            {
+                string input = Console.ReadLine();
+                Product product = new Product(input);
+                if (cart.ContainsKey(product.Id))
+                    cart[product.Id] += product.Count;
+                else
+                    cart.Add(product.Id, product.Count);
+            }
+            var products = (from item in cart select new Product(item.Key, item.Value)).ToList();
+            Console.ReadLine();
         }
     }
 }
