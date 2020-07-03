22ca97b735f8aef1b978d3c6d3fbcd52a001a45a
raghudotnet32 <Jagadesh.Jolapalli@appliedis.com>
2020-07-03 14:18:50 +0530

    Added code to ProductB and test cases

--- Promotion.Calculator/Products/ProductB.cs
+++ Promotion.Calculator/Products/ProductB.cs
@@ -5,14 +5,27 @@ namespace Promotion.Calculator.Products
 {
     public class ProductB<T> : IPriceCalculator<T>, IPromotion<T> where T : List<Product>
     {
+        private readonly int price = 30;
+
+        public ProductB(T tType)
+        {
+            this.product = tType.Where(x => x.Id == "B").SingleOrDefault();
+        }
+
+        private readonly Product product;
+
         public int ApplyPromotion()
         {
-            throw new System.NotImplementedException();
+            return (product.Count / 2) * 45 + (product.Count % 2 * price);
         }
 
         public int CalculateProductPrice()
         {
-            throw new System.NotImplementedException();
+            if (product is null)
+                return 0;
+            if (product.Count < 2)
+                return product.Count * price;
+            return ApplyPromotion();
         }
     }
 }
---
+++ Tests/UnitTests/ProductBTests.cs
@@ -0,0 +1,45 @@
+﻿using Promotion.Calculator.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Promotion.Calculator.Tests
+{
+    public class ProductBTest : IDisposable
+    {
+
+        [Fact(DisplayName = "ProductB TotalPrice returns value")]
+        public void ProductBTotalPrice_Returns_Value()
+        {
+            //Arrange
+            DataHelper data = new DataHelper();
+            var products = data.GenerateData("B", 2);
+            //Assert
+            var value = new ProductB<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(45);
+        }
+
+        [Fact(DisplayName = "ProductB TotalPrice returns zero")]
+        public void ProductBTotalPrice_Returns_Zero()
+        {
+            //Arrange
+            DataHelper data = new DataHelper();
+            var products = data.GenerateData("A", 3);
+
+            //Assert
+            var value = new ProductB<List<Product>>(products).CalculateProductPrice();
+
+            //Act
+            value.Equals(0);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
+
